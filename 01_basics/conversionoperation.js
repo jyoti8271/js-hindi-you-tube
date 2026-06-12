8469f34289b6a441f4bc7683468d8ae2b400d1c6
jyoti8271 <jyoti8271singh@gmail.com>
2026-06-12 09:33:55 +0000

    Conversion Operation

--- 01_basics/conversionoperation.js
+++ 01_basics/conversionoperation.js
@@ -23,4 +23,57 @@ console.log(booleanLoggedin)
 let someNumber=89
 let stringNumber =String(someNumber)
 console.log(stringNumber);
-console.log(typeof(stringNumber))
+console.log(typeof stringNumber)
+
+
+
+
+//***************Operations............. */
+
+let value=3
+let negValue= -value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-1)
+console.log(2*3)
+console.log(2**3)
+console.log(2%4)
+
+
+let str1 ="hello "
+let str2="world"
+let str3=str1+str2
+
+console.log(str3);
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log((3+4)*5 % 3);
+
+console.log(true);
+console.log(+true);//possible
+console.log(+"");
+
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gamecounter=100
+gamecounter++;
+++gamecounter
+console.log(gamecounter);
+
+
+//prefix
+x=3
+const y=++x
+console.log(x,y)
+
+//postfix
+x=3
+const z = x++
+console.log(x,y)
+
+
+
