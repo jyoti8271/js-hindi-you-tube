fe95276f26eae962946efe89e3e7c1b0e89e446c
jyoti8271 <jyoti8271singh@gmail.com>
2026-06-12 07:55:54 +0000

    DataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const account_Id =14453
+let account_Email="jyoti8271singh@gmail.com"
+var accountpassword="12345"
+accountcity="jaipur"
+let accountState;
+
+//account_Id=2  --> not allwoed
+
+account_Email="stgi.@gmailcom"
+accountcity="manipur"
+
+console.log(account_Id);
+console.table([account_Id,account_Email,accountpassword,accountcity,accountState])
+
+/*
+prefer not to use var because 
+of issue in block scope and functional scope
+*/
