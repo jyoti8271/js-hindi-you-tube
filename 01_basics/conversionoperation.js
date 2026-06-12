9a93c6a9a57378fc81b3368dd0cb2366aaa84f45
jyoti8271 <jyoti8271singh@gmail.com>
2026-06-12 09:02:49 +0000

    conversionoperation

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,26 @@
+/*let score="33abc" //undefined,null show that the nan in output
+
+console.log(typeof score);
+console.log(typeof(score));
+ 
+let valueinnumber= Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);*/
+
+
+//"33" =>33
+//"33qbc =>nan"
+//true =>1; false=>0
+
+let isLoggedIn=1
+let booleanLoggedin=Boolean(isLoggedIn)
+console.log(booleanLoggedin)
+
+//1 =>true;0=> false
+//" " =>false
+// "hitesh" =>true
+
+let someNumber=89
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber))
