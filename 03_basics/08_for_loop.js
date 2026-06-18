40a9050807ebba9502cf2e554d7ddc063c30dfc5
jyoti8271 <jyoti8271singh@gmail.com>
2026-06-18 08:42:43 +0000

    1 project

--- 03_basics/08_for_loop.js
+++ 03_basics/08_for_loop.js
@@ -18,6 +18,8 @@ for (let i=1; i<=10; i++)
 
 
 
+
+
 /////
 for(i=0;i<=10;i++)
 {
@@ -55,3 +57,78 @@ for(let i=0; i<=10; i++)
 
 console.log(`value of i is ${i}`)    
 }
+
+
+
+
+//............................practice question............................................
+//1 to 10
+
+for(let i=0;i<=10;i++)
+{
+    const num=i
+    console.log(num)
+}
+
+//sum of 1 to 10 number
+sum=0;
+for(let i=1;i<=10;i++)
+{
+    
+    sum+=i
+}
+console.log(sum)
+
+/////even number print from 1 to 20
+for(let i=0;i<=20;i++)
+{
+    if(i%2===0)
+    {   
+        console.log(i)
+    }
+    else{   
+        console.log(i)
+    }
+}
+
+///table of 5
+for(let i=0;i<=10;i++)
+{
+    console.log(5*i)
+}
+
+//print array elements
+arr=["sjik","hgdhd","jdh"]
+for(let i=0;i<arr.length;i++)
+{
+    
+    console.log(arr[i])
+}
+
+
+////sum of array
+
+/*const arr = [1, 2, 3, 45];
+
+let sum5= 0;
+
+for(let i = 0; i < arr.length; i++) {
+    sum5+= arr[i];
+}
+
+console.log(sum5);*/
+
+
+
+////find largest number in array
+const large=[2,3,4,5,6]
+let arr[0]
+for(let i=0;i<=length;i++)
+{
+    if(i>large)
+    {
+        console.log(large)
+    }
+}
+
+
